d974d8c177168685eda0426c5bc3fecc8cf55b63
People <s3467585@gmail.com>
2020-09-11 16:53:52 +0500

    11.09.2020 send mail is ok

--- site/mail.php
+++ site/mail.php
@@ -1,32 +1,65 @@
 <?php
-	
-	header('Content-Type: text/html; charset=utf-8');
-	$key = protect($_GET["k"]);
-	
-	echo '<br>' . $key;
-	echo 'test';
-	if ($key == 1){
-		$to = "s3467585@gmail.com";
-	} else {
-		$to = "_chelovek_@mail.ru";
+	// Файлы phpmailer
+	require 'lib/phpmailer/PHPMailer.php';
+	require 'lib/phpmailer/SMTP.php';
+	require 'lib/phpmailer/Exception.php';
+
+	// Переменные, которые отправляет пользователь
+	$name = 'name';
+	$email = 'email';
+	$text = 'text';
+	$file = 'myfile';
+
+	// Формирование самого письма
+	$title = "Заголовок письма";
+	$body = "
+		<h2>Новое письмо</h2>
+		<b>Имя:</b> $name<br>
+		<b>Почта:</b> $email<br><br>
+		<b>Сообщение:</b><br>$text";
+
+	// Настройки PHPMailer
+	$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+	try {
+	    $mail->isSMTP();   
+	    $mail->CharSet = "UTF-8";
+	    $mail->SMTPAuth   = true;
+	    //$mail->SMTPDebug = 2;
+	    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+	    // Настройки вашей почты
+	    $mail->Host       = 'smtp.mail.ru'; 					// SMTP сервера вашей почты
+	    $mail->Username   = 'server-2020-86@mail.ru'; 		// Логин на почте
+	    $mail->Password   = 'T2yYuOUp4au&';                     // Пароль на почте
+	    $mail->SMTPSecure = 'ssl';
+	    $mail->Port       = 465;
+	    $mail->setFrom('server-2020-86@mail.ru', 'SovHome'); 	// Адрес самой почты и имя отправителя
+
+	    // Получатель письма
+	    $mail->addAddress('s3467585@gmail.com');  
+	    $mail->addAddress('_chelovek_@mail.ru');    				// Ещё один, если нужен
+
+	// Отправка сообщения
+	$mail->isHTML(true);
+	$mail->Subject = $title;
+	$mail->Body = $body;    
+
+	// Проверяем отравленность сообщения
+	if ($mail->send()) {$result = "success";} 
+	else {$result = "error";}
+
+	} catch (Exception $e) {
+	    $result = "error";
+	    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 	}
-	echo $to;
 
-	/*$objekt = 'Температура';
-	$int = '10';
+	// Отображение результата
+	echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
 
-	$subject = 'SovHome - Внимание';
-	$message = 'Внимание!';
-	echo '<br>'.$message;
-	$headers = array(
-	    'From' => 'sovhomec@sovhome.cu.ma',
-	    'Reply-To' => 'sovhomec@sovhome.cu.ma',
-	    'X-Mailer' => 'PHP/'.phpversion()
-	);
-
-	$success = mail($to, $subject, $message, $headers);
-	if (!$success){
-		$errorMessage = error_get_last()['message'];
-		echo '<br>Ошибка отправки Mail';
-	} */
 ?>
+
+
+
+
+
